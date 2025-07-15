9d3fd57f12d23804b17c7e44d7cb66116aa9146a
Epitomaniac <d.mohebi1993@gmail.com>
2025-07-15 22:45:30 +0330

    chore: added promotion story

--- src/stories/Promorion.stories.tsx
+++ src/stories/Promorion.stories.tsx
@@ -1,7 +1,9 @@
+// @ts-nocheck
+import { useState, useEffect, useRef } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { Chessboard } from '../../src';
-import { useState } from 'react';
-import type { Highlight, SquareArrowHandlerArgs } from '../../src/types';
+import { Chess } from 'chess.js';
+import { PieceDropHandlerArgs } from '../../src';
 
 const meta: Meta<typeof Chessboard> = {
   title: 'stories/Promotion',
@@ -13,12 +15,69 @@ type Story = StoryObj<typeof meta>;
 
 export const Promotion: Story = {
   render: () => {
-    const [position, setPosition] = useState('8/1P6/5K1k/8/8/8/8/8 w - - 0 1');
+    const chessGameRef = useRef(new Chess('8/2P2k2/8/8/3K4/8/8/8 w - - 0 1'));
+    const chessGame = chessGameRef.current;
+
+    const [chessPosition, setChessPosition] = useState(chessGame.fen());
+    const [promotionDialog, setPromotionDialog] = useState('none');
+    const [promotionPiece, setPromotionPiece] = useState<string | undefined>(
+      undefined,
+    );
+    const [promotionSource, setPromotionSource] = useState('');
+    const [promotionTarget, setPromotionTarget] = useState('');
+
+    function makeMove({ piece, sourceSquare, targetSquare }) {
+      if (!targetSquare) {
+        return false;
+      }
+
+      console.log(piece?.pieceType, sourceSquare, targetSquare);
+
+      if (
+        (piece?.pieceType == 'wP' && targetSquare.includes('8')) ||
+        (piece?.pieceType == 'bP' && targetSquare.includes('1'))
+      ) {
+        setPromotionSource(sourceSquare);
+        setPromotionTarget(targetSquare);
+        console.log('dialog is set');
+        setPromotionDialog('modal');
+        return false;
+      }
+
+      chessGame.move({
+        from: sourceSquare,
+        to: targetSquare,
+        promotion: promotionPiece ?? 'q',
+      });
+
+      // update the position state upon successful move to trigger a re-render of the chessboard
+      setChessPosition(chessGame.fen());
+      // return true as the move was successful
+      return true;
+    }
+
+    function handlePromotionPieceSelect(piece: string) {
+      setPromotionPiece(piece[1].toLowerCase());
+      setPromotionDialog('none');
+    }
+
+    useEffect(() => {
+      if (promotionPiece) {
+        makeMove({
+          sourceSquare: promotionSource,
+          targetSquare: promotionTarget,
+        });
+        setPromotionSource(null);
+        setPromotionTarget(null);
+      }
+    }, [promotionPiece]);
 
     const chessboardOptions = {
       id: 'promotion',
-      position,
-      promotionDialog: 'modal',
+      position: chessPosition,
+      onPieceDrop: makeMove,
+      onPromotionPieceSelect: handlePromotionPieceSelect,
+      promotionDialog,
     };
 
     return (
